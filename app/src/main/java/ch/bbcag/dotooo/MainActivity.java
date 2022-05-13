3b84a45c6161b0b3b0c149fef460997a4197341f
Mattia Gisiger <bgisim@bbcag.ch>
2022-05-13 13:14:54 +0200

    details activity working

--- app/src/main/java/ch/bbcag/dotooo/MainActivity.java
+++ app/src/main/java/ch/bbcag/dotooo/MainActivity.java
@@ -2,7 +2,10 @@ package ch.bbcag.dotooo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -33,10 +36,18 @@ public class MainActivity extends AppCompatActivity {
         task2.setDescription("Shessh what a task");
         taskDao.insert(task2);
 
-
-
-
         arrayAdapter.addAll(taskDao.getAll());
         listView.setAdapter(arrayAdapter);
+
+        AdapterView.OnItemClickListener mListClickHandler = (parent, v, position, id) -> {
+            Intent intent = new Intent(getApplicationContext(), TaskActivity.class);
+            Task selected = (Task)parent.getItemAtPosition(position);
+
+            intent.putExtra("taskId", selected.getId());
+            intent.putExtra("taskTitle", selected.getTitle());
+            intent.putExtra("taskDescription", selected.getDescription());
+            startActivity(intent);
+        };
+        listView.setOnItemClickListener(mListClickHandler);
     }
 }
