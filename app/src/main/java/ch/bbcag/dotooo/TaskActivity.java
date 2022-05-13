665a7e065783576def8b0f495be4700d3a60afcd
Mattia Gisiger <bgisim@bbcag.ch>
2022-05-13 13:38:16 +0200

    up button in TaskActivity working

--- app/src/main/java/ch/bbcag/dotooo/TaskActivity.java
+++ app/src/main/java/ch/bbcag/dotooo/TaskActivity.java
@@ -1,8 +1,10 @@
 package ch.bbcag.dotooo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class TaskActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class TaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
