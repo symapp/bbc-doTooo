00bc7c3dc716f5d53611a915172cab9c802f14d2
bgisim <bgisim@bbcag.ch>
2022-05-13 09:31:55 +0200

    added imports and dependencies

--- app/src/main/java/ch/bbcag/dotooo/dal/TaskRoomDatabase.java
+++ app/src/main/java/ch/bbcag/dotooo/dal/TaskRoomDatabase.java
@@ -6,6 +6,8 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import ch.bbcag.dotooo.Entity.Task;
+
 @Database(
         entities = {Task.class},
         version = 1
