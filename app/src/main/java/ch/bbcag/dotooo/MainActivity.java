66ebd630841acd9dbf1e0fd3b02986d18126a85a
Zehnder Levin <bzehnl@bbcag.ch>
2022-05-13 11:04:42 +0200

    Task from database is shown on mainActivity

--- app/src/main/java/ch/bbcag/dotooo/MainActivity.java
+++ app/src/main/java/ch/bbcag/dotooo/MainActivity.java
@@ -3,12 +3,36 @@ package ch.bbcag.dotooo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import ch.bbcag.dotooo.dal.TaskRoomDao;
+import ch.bbcag.dotooo.dal.TaskRoomDatabase;
+import ch.bbcag.dotooo.entity.Task;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.list);
+        ArrayAdapter<Task> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+
+        TaskRoomDatabase database = TaskRoomDatabase.getInstance(getApplicationContext());
+        TaskRoomDao taskDao= database.getTaskDao();
+
+        Task task1 = new Task();
+        task1.setId(1);
+        task1.setTitle("First Task");
+        task1.setDescription("asdfasdfasdf");
+
+
+        taskDao.insert(task1);
+
+        arrayAdapter.add(task1);
+        listView.setAdapter(arrayAdapter);
     }
 }
